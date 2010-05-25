f4fedcde0ee99fef99512b26c63eeb34bdce0fc9
James Hunt and Richard Patching <dev+ohthatjames+richardeden@edendevelopment.co.uk>
2010-05-25 12:11:01 +0100

    Adjust migration to use person instead of student

--- db/migrate/20100524151953_move_notes_to_comments_in_students.rb
+++ db/migrate/20100524151953_move_notes_to_comments_in_students.rb
@@ -2,13 +2,13 @@ class MoveNotesToCommentsInStudents < ActiveRecord::Migration
   class Comment < ActiveRecord::Base
 
   end
-  class Student < ActiveRecord::Base
+  class Person < ActiveRecord::Base
 
   end
   def self.up
     user = User.first
     if user
-      Student.all.each do |student|
+      Person.all.each do |student|
         next if student.notes.blank?
         Comment.create!(:commentable_id => student.id, :commentable_type => "Student", :comment => student.notes, :user_id => user.id)
       end
@@ -18,7 +18,7 @@ class MoveNotesToCommentsInStudents < ActiveRecord::Migration
 
   def self.down
     add_column :students, :notes, :text
-    Student.all.each do |student|
+    Person.all.each do |student|
       comments = Comment.find(:all, :conditions => {:commentable_type => "Student", :commentable_id => student.id}, :order => "created_at asc")
       student.update_attributes :notes => comments.map(&:comment).join("\n\n")
       comments.each(&:destroy)
