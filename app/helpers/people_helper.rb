2a84e705d1f94de7d86ecadc6e129498c92a9c14
James Hunt and Richard Patching <dev+ohthatjames+richardeden@edendevelopment.co.uk>
2010-05-25 15:36:33 +0100

    Refactor person edit forms into helper

---
+++ app/helpers/people_helper.rb
@@ -0,0 +1,9 @@
+module PeopleHelper
+  def person_form_for(type_name)
+    concat content_tag(:h1, "Edit #{type_name} for #{@person.name}")
+    form_for @person, :html => { :multipart => true } do |form|
+      yield form
+      concat form.submit("Save")
+    end
+  end
+end
