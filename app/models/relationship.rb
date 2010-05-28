b6b522ea647312f4fa7c9a670ff7d659d1326549
David Henry and Richard Patching <dev+dwhenry+richardeden@edendevelopment.co.uk>
2010-05-28 14:54:11 +0100

    Add check to ensure caretaker is unique when adding to a person.

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -24,6 +24,11 @@ class Relationship < ActiveRecord::Base
   end
   
   def ensure_unique_relationship
+    ensure_unique_caretaker if caretaker?
     errors.add_to_base "These two people already have that relationship" if Relationship.including_people(from, to, relationship_type)
+  end
+  
+  def ensure_unique_caretaker
+    errors.add_to_base "This person is already a caretaker" if Relationship.find(:first, :conditions => {:from_id => from.id, :to_id => to.id, :caretaker => true})
   end
 end
