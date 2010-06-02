7565c710578cac9dd40607d558f2302c172bbe81
James Hunt <ohthatjames@googlemail.com>
2010-06-02 14:27:24 +0100

    Don't show blank fields on person profile

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -26,7 +26,9 @@ module PeopleHelper
     end
     
     def text(field_name, options = {})
-      output(field_name, simple_format(@object.send(field_name)), options)
+      value = @object.send(field_name)
+      return nil if value.blank?
+      output(field_name, simple_format(value), options)
     end
     
     def boolean(field_name, options = {})
@@ -34,6 +36,7 @@ module PeopleHelper
     end
     
     def output(field_name, field_value, options = {})
+      return nil if field_value.blank?
       options[:label_name] ||= field_name.humanize
       options[:class_name] ||= options[:label_name].parameterize
       
