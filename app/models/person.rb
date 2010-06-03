8d8bbd297d5236786594a8fdbcf1814638187509
Enrique Comba Riepenhausen and James Hunt <dev+ecomba+ohthatjames@edendevelopment.co.uk>
2010-06-03 12:31:33 +0100

    use constant instead of string for sibling relationship_type

--- app/models/person.rb
+++ app/models/person.rb
@@ -52,17 +52,17 @@ class Person < ActiveRecord::Base
   end
   
   def add_sibling(sibling)
-    Relationship.create :from => self, :to => sibling, :relationship_type => "sibling"
+    Relationship.create :from => self, :to => sibling, :relationship_type => Relationship::SIBLING
   end
   
   def siblings
-    Relationship.find(:all, :conditions => ["(from_id = :id OR to_id = :id) AND relationship_type = 'sibling'", {:id => self.id}]).map do |relationship|
+    Relationship.find(:all, :conditions => ["(from_id = :id OR to_id = :id) AND relationship_type = :type", {:id => self.id, :type => Relationship::SIBLING}]).map do |relationship|
       relationship.other_half(self)
     end
   end
   
   def remove_sibling(sibling)
-    Relationship.including_people(self, sibling, 'sibling').destroy
+    Relationship.including_people(self, sibling, Relationship::SIBLING).destroy
   end
   
   def add_caretaker(caretaker)
