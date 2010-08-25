70336099a412dfd393c68681dcbcf064b2be561d
aimee daniells and Steve Tooke <dev+sermoa+tooky@edendevelopment.co.uk>
2010-08-25 12:48:08 +0100

    Tests for the pagination methods separately from the application.

--- vendor/plugins/paginate_alphabetically/init.rb
+++ vendor/plugins/paginate_alphabetically/init.rb
@@ -1,3 +1,3 @@
-require 'paginate_alphabetically'
+require File.expand_path(File.join(File.dirname(__FILE__), 'lib/paginate_alphabetically'))
 
 ActiveRecord::Base.extend(PaginateAlphabetically)
--- vendor/plugins/paginate_alphabetically/test/paginate_alphabetically_test.rb
+++ vendor/plugins/paginate_alphabetically/test/paginate_alphabetically_test.rb
@@ -1,8 +1,36 @@
-require 'test_helper'
+require File.expand_path(File.join(File.dirname(__FILE__), 'test_helper'))
+
+class Thing < ActiveRecord::Base
+  paginate_alphabetically :by => :name
+end
 
 class PaginateAlphabeticallyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    %w(One Two Three Four Five Six).map {|name| Thing.create!(:name => name)}
+  end
+
+  def test_pagination_letters
+    assert_equal ['F', 'O', 'S', 'T'], Thing.pagination_letters
+  end
+
+  def test_first_letter
+    assert_equal 'F', Thing.first_letter
+  end
+
+  def test_first_letter_when_there_are_no_things
+    Thing.destroy_all
+    assert_equal 'A', Thing.first_letter
+  end
+
+  def test_alphabetical_group_without_specifying_letter
+    assert_equal ['Five', 'Four'], Thing.alphabetical_group.map(&:name)
+  end
+
+  def test_alphabetical_group_specifying_letter
+    assert_equal ['Three', 'Two'], Thing.alphabetical_group('t').map(&:name)
+  end
+
+  def teardown
+    Thing.destroy_all
   end
 end
