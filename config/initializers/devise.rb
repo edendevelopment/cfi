6aa064fdb18c0b496c08d021aa3092b3aefd7101
Louis Rose <louismrose@gmail.com>
2010-09-27 18:47:31 +0100

    Fixed Devise deprecation warning: set encryptor in initialiser.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,7 +20,7 @@ Devise.setup do |config|
   # authentication tools as :clearance_sha1, :authlogic_sha512 (then you should set
   # stretches above to 20 for default behavior) and :restful_authentication_sha1
   # (then you should set stretches to 10, and copy REST_AUTH_SITE_KEY to pepper)
-  # config.encryptor = :sha1
+  config.encryptor = :sha1
 
   # Configure which keys are used when authenticating an user. By default is
   # just :email. You can configure it to use [:username, :subdomain], so for
