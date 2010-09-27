e265926b6485b3e8e948741da1757e191c707b5f
Louis Rose <louismrose@gmail.com>
2010-09-27 17:30:02 +0100

    Fixed photos#create: inherited_resources requires a call to create! for photo to be created.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,6 @@ class PhotosController < InheritedResources::Base
   belongs_to :person
   
   def create
-    redirect_to person_photos_path(@person)
+    create! { person_photos_path(@person) }
   end
 end
