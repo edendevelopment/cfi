4b1c6296bb0bb120f00ed5d5828dd42d71345fcf
Chris Parsons <chris@edendevelopment.co.uk>
2010-10-08 23:26:08 +0100

    Added compiled stylesheets in for heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -3,3 +3,4 @@ require 'compass'
 Compass.add_project_configuration(File.join(Rails.root.to_s, "config", "compass.config"))
 Compass.configuration.environment = Rails.env.to_sym
 Compass.configure_sass_plugin!
+Sass::Plugin.options[:never_update] = true
