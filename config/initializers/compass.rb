1199bb17645ad0e15e50d4e38ce3f2c8ea744a49
Chris Parsons <chris@edendevelopment.co.uk>
2010-11-10 08:29:18 +0000

    Fix stylesheets on heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -4,3 +4,8 @@ Compass.add_project_configuration(File.join(Rails.root.to_s, "config", "compass.
 Compass.configuration.environment = Rails.env.to_sym
 Compass.configure_sass_plugin!
 Sass::Plugin.options[:never_update] = false
+
+# Make stylesheets compile to tmp
+require "fileutils"
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
+ActionController::Dispatcher.middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets/compiled"])
