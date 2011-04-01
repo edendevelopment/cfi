9fdc998db499efe0679380229ce154499fd233fc
Chris Parsons <chris@edendevelopment.co.uk>
2011-04-01 16:06:31 +0100

    Fixed dragonfly on staging server

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -3,6 +3,6 @@ require 'dragonfly'
 app = Dragonfly[:images]
 app.configure_with(:rmagick)
 app.configure_with(:rails)
-app.configure_with(:heroku, ENV['S3_BUCKET']) if Rails.env.production?
+app.configure_with(:heroku, ENV['S3_BUCKET']) unless Rails.env.development?
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
