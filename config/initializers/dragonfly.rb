17911b8e1bf0135a7717df7a750b97f041f2b3fe
Chris Parsons <chris@thinkcodelearn.com>
2011-06-26 23:02:48 +0100

    Specs passing, cukes failing

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -3,6 +3,6 @@ require 'dragonfly'
 app = Dragonfly[:images]
 app.configure_with(:rmagick)
 app.configure_with(:rails)
-app.configure_with(:heroku, ENV['S3_BUCKET']) unless Rails.env.development?
+app.configure_with(:heroku, ENV['S3_BUCKET']) unless Rails.env.development? || Rails.env.test?
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
